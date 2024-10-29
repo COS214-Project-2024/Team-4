964867ece43d428235c9fffd5db3f41ca4bb31bd
Naven13 <u21512494@tuks.co.za>
2024-10-29 10:05:29 +0200

    Add files via upload

---
+++ ResourceManager.cpp
@@ -0,0 +1,40 @@
+#include "..\..\Users\naven\AppData\Roaming\VisualParadigm\ws\teamwork_client\projects\COS214_Prac6_Class_Diagram\ResourceManager.h"
+
+bool ResourceManager::allocateResources(String type, int quantity) {
+	// TODO - implement ResourceManager::allocateResources
+	throw "Not yet implemented";
+}
+
+void ResourceManager::releaseResources(String type, int quantity) {
+	// TODO - implement ResourceManager::releaseResources
+	throw "Not yet implemented";
+}
+
+void ResourceManager::attachObserver(Observer obs) {
+	// TODO - implement ResourceManager::attachObserver
+	throw "Not yet implemented";
+}
+
+void ResourceManager::notifyObservers() {
+	// TODO - implement ResourceManager::notifyObservers
+	throw "Not yet implemented";
+}
+
+void ResourceManager::setAllocationStrategy(ResourceAllocationStrategy strategy) {
+	// TODO - implement ResourceManager::setAllocationStrategy
+	throw "Not yet implemented";
+}
+
+Resource ResourceManager::getResource(ResourceType type) {
+	// TODO - implement ResourceManager::getResource
+	throw "Not yet implemented";
+}
+
+bool ResourceManager::deductBudget(int cost) {
+	// TODO - implement ResourceManager::deductBudget
+	throw "Not yet implemented";
+}
+
+int ResourceManager::getBudget() {
+	return this->budget;
+}
