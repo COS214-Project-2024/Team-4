4a394aed32c0839c7e6329477937c54561f51a6c
Zainab <abdulrasaqzainab0@gmail.com>
2024-10-31 10:44:41 +0200

    moving files

---
+++ src/ResidentialBuilding.h
@@ -0,0 +1,36 @@
+#ifndef RESIDENTIALBUILDING_H
+#define RESIDENTIALBUILDING_H
+#include "Building.h"
+//#include "Citizen.h"
+#include<iostream>
+#include<string>
+using namespace std;
+
+class ResidentialBuilding :public Building {
+
+private:
+	 int residentialUnits = 0;
+    float comfortLevel = 0.0f;
+	string bType;
+
+
+public:
+	 ResidentialBuilding(const std::string& name, float area, int floors, int capacity,
+                        float citizenSatisfaction, float economicGrowth, float resourceConsumption,
+                        int residentialUnits, float comfortLevel);
+
+	string getType() const override;
+
+	void updateImpacts()override;
+	void construct() override;
+	void upgradeComfort(float comfort);
+
+protected:
+	void calculateEconomicImpact();
+
+	void calculateResourceConsumption();
+
+	void calculateSatisfactionImpact();
+};
+
+#endif
