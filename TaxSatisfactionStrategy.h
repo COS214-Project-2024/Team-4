c8fc1b63dc5383e839082af1cd964bef53857688
blizs <67783290+blizs@users.noreply.github.com>
2024-11-01 13:23:18 +0200

    Add files via upload

---
+++ TaxSatisfactionStrategy.h
@@ -0,0 +1,15 @@
+// TaxSatisfactionStrategy.h
+
+#ifndef TAXSATISFACTIONSTRATEGY_H
+#define TAXSATISFACTIONSTRATEGY_H
+
+#include "SatisfactionStrategy.h"
+#include "Citizen.h"
+
+class TaxSatisfactionStrategy : public SatisfactionStrategy {
+public:
+    float calculateSatisfaction(const Citizen& citizen) const override;
+};
+
+#endif // TAXSATISFACTIONSTRATEGY_H
+
