64d1357db5d40bfc0da4a619c560eef2b8cc3dfc
blizs <67783290+blizs@users.noreply.github.com>
2024-11-01 13:34:22 +0200

    Delete EmployedState.cpp

--- EmployedState.cpp
+++
@@ -1,8 +0,0 @@
-#include "EmployedState.h"
-#include "Citizen.h"
-
-void EmployedState::handleState(Citizen& citizen) const {
-    // Increase satisfaction due to employment
-    citizen.updateSatisfaction(citizen.getSatisfactionLevel() + 10.0);
-}
-
