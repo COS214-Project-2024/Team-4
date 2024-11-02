b54608d961cb941055f3f2b123d372f446f4ff28
Neo_acer_wsl <u23002167@tuks.co.za>
2024-11-02 22:01:40 +0200

    Sewagemangement: changed Resourcetype name from sewagecapacity to just sewage (helps keep track with shorter name)

--- src/SewageManagement.cpp
+++ src/SewageManagement.cpp
@@ -8,7 +8,7 @@ void SewageManagement::registerBuilding(Building* building) {
 }
 
 void SewageManagement::supplyResources(Building* building) {
-    if (mediator->requestResources(ResourceType::SewageCapacity, 20)) {
+    if (mediator->requestResources(ResourceType::Sewage, 20)) {
         std::cout << "Sewage management service provided to building.\n";
     } else {
         std::cout << "Insufficient sewage capacity for building.\n";
