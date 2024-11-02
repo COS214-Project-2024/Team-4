f7b2e5508a15534563397eaf7492e28ccc5883d5
Rudolph <52330699+RudolphLamp@users.noreply.github.com>
2024-11-02 18:55:47 +0200

    Add files via upload

--- src/SetTaxCommand.h
+++ src/SetTaxCommand.h
@@ -4,17 +4,24 @@
 #include "GovCommand.h"
 #include "Government.h"
 
+// SetTaxCommand class
+// This class represents a command to set the tax rate in the government
 class SetTaxCommand : public GovCommand {
 
 private:
-    Government* government;
-    double taxRate;
-    double previousTaxRate;
+    Government* government;   // Pointer to the government object
+    double taxRate;           // New tax rate to be set
+    double previousTaxRate;   // Previous tax rate to revert if needed
 
 public:
+    // Constructor
+    // Initializes the command with the government object and the new tax rate
     SetTaxCommand(Government* gov, double rate);
 
+    // Executes the set tax command
     void execute() override;
+
+    // Undoes the set tax command
     void undo() override;
 };
 
