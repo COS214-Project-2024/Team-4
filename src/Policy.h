eac249694243a45b46986b6d5760cb5838aae9d0
Rudolph <52330699+RudolphLamp@users.noreply.github.com>
2024-11-02 18:56:16 +0200

    Add files via upload

--- src/Policy.h
+++ src/Policy.h
@@ -1,16 +1,19 @@
 #ifndef POLICY_H
 #define POLICY_H
-//policy
+
 #include <string>
 
 class Policy {
 
 private:
-    std::string policyName;
-    std::string impactLevel;
+    std::string policyName;   // Name of the policy
+    std::string impactLevel;  // Impact level of the policy
 
 public:
+    // Implements the policy
     void implement();
+
+    // Revokes the policy
     void revoke();
 };
 
