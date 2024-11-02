4101ecc2a2c6123b7d3aa00c7b1f33da5360aa68
Rudolph <52330699+RudolphLamp@users.noreply.github.com>
2024-11-02 18:55:25 +0200

    Add files via upload

--- src/AllocateBudgetCommand.h
+++ src/AllocateBudgetCommand.h
@@ -1,22 +1,29 @@
 #ifndef ALLOCATEBUDGETCOMMAND_H
 #define ALLOCATEBUDGETCOMMAND_H
-// COMMAND DP
+
 #include "GovCommand.h"
 #include "Government.h"
 #include "CityService.h"
 
+// AllocateBudgetCommand class
+// This class represents a command to allocate a budget to a city service
 class AllocateBudgetCommand : public GovCommand {
 
 private:
-    Government* government;
-    CityService& service;
-    double amount;
-    double prevAllocation;
+    Government* government; // Pointer to the government object
+    CityService& service;   // Reference to the city service object
+    double amount;          // Amount to allocate
+    double prevAllocation;  // Previous allocation amount
 
 public:
+    // Constructor
+    // Initializes the command with the government, city service, and the amount to allocate
     AllocateBudgetCommand(Government* gov, CityService& srv, double amt);
 
+    // Executes the budget allocation command
     void execute() override;
+
+    // Undoes the budget allocation command
     void undo() override;
 };
 
