a145bb6e71df405ffd20f634f8d911e795cf5805
Rudolph <52330699+RudolphLamp@users.noreply.github.com>
2024-11-03 20:39:36 +0200

    CollectTAX added - GOV

--- src/CollectTaxesCommand.cpp
+++ src/CollectTaxesCommand.cpp
@@ -1,24 +1,62 @@
 #include "CollectTaxesCommand.h"
+#include <stdexcept>
 
-CollectTaxesCommand::CollectTaxesCommand(Building* building, TaxType* taxType) {
-	this->building = building;
-	taxesCollected = 0;
-	this->taxType = taxType;
-}
+// Constructor for CollectTaxesCommand
+// Initializes the command with the government object
+CollectTaxesCommand::CollectTaxesCommand(Government* gov)
+    : government(gov), collectedTaxes(0.0) {}
 
+// Executes the tax collection command
+// Collects taxes from the government and stores the collected amount
 void CollectTaxesCommand::execute() {
-    taxesCollected = building->payTaxes(taxType);
-    std::cout << "Total taxes collected by CollectTaxesCommand: $" << taxesCollected << "\n";
+    collectedTaxes = government->collectTaxes();
 }
 
-
+// Undoes the tax collection command
+// Refunds the collected taxes to the government
 void CollectTaxesCommand::undo() {
-	building->undoCollectTaxes();
-}
-double CollectTaxesCommand::getTaxesCollected() {
-	return taxesCollected;
+    government->refundTaxes(collectedTaxes);
 }
 
-double CollectTaxesCommand::returnVal() {
-	return taxesCollected;
+// Gets the amount of taxes collected
+double CollectTaxesCommand::getCollectedTaxes() const {
+    return collectedTaxes;
+}
+
+// Sets the amount of taxes collected
+void CollectTaxesCommand::setCollectedTaxes(double amount) {
+    if (amount < 0) {
+        throw std::invalid_argument("Collected taxes cannot be negative");
+    }
+    collectedTaxes = amount;
+}
+
+// Validates the tax collection
+// Ensures the government has a valid tax rate
+bool CollectTaxesCommand::validateCollection() const {
+    return government->getTaxRate() > 0;
+}
+
+// Executes the tax collection command with validation
+// Throws an exception if the collection is invalid
+void CollectTaxesCommand::executeWithValidation() {
+    if (!validateCollection()) {
+        throw std::runtime_error("Invalid tax rate for collection");
+    }
+    execute();
+}
+
+// Gets the name of the command
+std::string CollectTaxesCommand::getName() const {
+    return "CollectTaxesCommand";
+}
+
+// Gets the description of the command
+std::string CollectTaxesCommand::getDescription() const {
+    return "Collects taxes from the government.";
+}
+
+// Checks if the command can be executed
+bool CollectTaxesCommand::canExecute() const {
+    return validateCollection();
 }
--- src/CollectTaxesCommand.h
+++ src/CollectTaxesCommand.h
@@ -3,21 +3,46 @@
 
 #include "GovCommand.h"
 #include "Government.h"
-#include "Building.h"
-#include "TaxType.h"
+
+// CollectTaxesCommand class
+// This class represents a command to collect taxes from the government
 class CollectTaxesCommand : public GovCommand {
+
 private:
-	Building* building;
-	double taxesCollected;
-	double taxRate;
-	TaxType* taxType;
+    Government* government;   // Pointer to the government object
+    double collectedTaxes;    // Amount of taxes collected
 
 public:
-	CollectTaxesCommand(Building* building, TaxType* taxType);
-	void execute();
-	void undo();
-	double getTaxesCollected();
-	double returnVal();
+    // Constructor
+    // Initializes the command with the government object
+    CollectTaxesCommand(Government* gov);
+
+    // Executes the tax collection command
+    void execute() override;
+
+    // Undoes the tax collection command
+    void undo() override;
+
+    // Gets the amount of taxes collected
+    double getCollectedTaxes() const;
+
+    // Sets the amount of taxes collected
+    void setCollectedTaxes(double amount);
+
+    // Validates the tax collection
+    bool validateCollection() const;
+
+    // Executes the tax collection command with validation
+    void executeWithValidation();
+
+    // Gets the name of the command
+    std::string getName() const override;
+
+    // Gets the description of the command
+    std::string getDescription() const override;
+
+    // Checks if the command can be executed
+    bool canExecute() const override;
 };
 
 #endif
