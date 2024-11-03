1b3ef1065a33a7658cdb8ad49719c5bed7b50146
Shayden Naidoo <140940045+ShaydenNaidoo@users.noreply.github.com>
2024-11-03 14:23:42 +0200

    Update SetTaxCommand.cpp

--- src/SetTaxCommand.cpp
+++ src/SetTaxCommand.cpp
@@ -1,32 +1,52 @@
 #include "SetTaxCommand.h"
+#include "Government.h"
+#include "TaxSystem.h"
+#include <iostream>
+#include "TaxType.h"
+#include "VAT.h"
+#include "Sales.h"
+#include "Income.h"
+#include "Property.h"
 
-// Constructor for SetTaxCommand
-// Initializes the command with the government object and the new tax rate
-SetTaxCommand::SetTaxCommand(Government* gov, double rate) : government(gov), taxRate(rate) {}
+SetTaxCommand::SetTaxCommand(Government* gov, TaxSystem* taxSys, double rate, char taxType)
+    : government(gov), taxSys(taxSys), taxRate(rate), taxType(taxType), previousTaxRate(0.0)  {
 
-// Executes the set tax command
-// Stores the previous tax rate and sets the new tax rate
+
+
+    if (taxType == 'I') {
+        newTaxType = new Income(rate);
+    } else if (taxType == 'P') {
+        double propertyLevy = 0.5;  //  levy for property tax
+        double additionalFees = 100.0; //  additional fees
+        newTaxType = new Property(rate, propertyLevy, additionalFees);
+    } else if (taxType == 'S') {
+        double salesLevy = 2.0; //  environmental levy for sales tax
+        double serviceFee = 15.0; //  service fee
+        newTaxType = new Sales(rate, salesLevy, serviceFee);
+    } else if (taxType == 'V') {
+        newTaxType = new VAT(rate);
+    } else {
+        std::cout << "Invalid tax type" << std::endl;
+        return; // Exit if tax type is invalid
+    }
+
+
+   
+}
 void SetTaxCommand::execute() {
-    previousTaxRate = government->getTaxRate();
-    government->setTax(taxRate);
+// Add the tax rate to the tax system
+    taxSys->addTaxRate(newTaxType);
 }
 
-// Undoes the set tax command
-// Reverts the tax rate to the previous rate
 void SetTaxCommand::undo() {
+    // Remove the tax rate from the tax system
+    taxSys->removeTaxRate(newTaxType);
     government->setTax(previousTaxRate);
 }
 
-// Gets the name of the command
-std::string SetTaxCommand::getName() const {
-    return "SetTaxCommand";
+double SetTaxCommand::returnVal() {
+    return newTaxType->getTaxRate();
 }
-
-// Gets the description of the command
-std::string SetTaxCommand::getDescription() const {
-    return "Sets the tax rate in the government.";
-}
-
 // Checks if the command can be executed
 bool SetTaxCommand::canExecute() const {
     return taxRate >= 0;
